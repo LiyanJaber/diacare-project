413e345ec721cbcbf33d2026642da414f2704df5
LiyanJaber <layan.j2005@gmail.com>
2026-04-10 02:14:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,79 @@
-$(document).ready(function(){
-    // Page load animation
-    $('body').hide().fadeIn(800);
+/* LOGIN */
+function loginUser(){
+    let email=document.getElementById("email").value;
+    let pass=document.getElementById("password").value;
+    let msg=document.getElementById("loginMsg");
 
-    // Navbar color change on scroll
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('.navbar').css('background-color', '#E6FFFA');
-        } else {
-            $('.navbar').css('background-color', '#ffffff');
+    if(email===""||pass===""){
+        msg.innerText="Fill all fields";
+        msg.style.color="red";
+        return;
+    }
+
+    if(pass.length<6){
+        msg.innerText="Password too short";
+        msg.style.color="red";
+        return;
+    }
+
+    localStorage.setItem("user",email);
+
+    msg.innerText="Login success!";
+    msg.style.color="green";
+
+    setTimeout(()=>window.location.href="index.html",1000);
+}
+
+
+/* TRACKER + DATA */
+let sugarData = [];
+
+function checkSugar(){
+    let val=parseFloat(document.getElementById("sugarInput").value);
+    let box=document.getElementById("resultBox");
+
+    if(isNaN(val)){
+        box.innerHTML="Enter valid number";
+        box.className="result high";
+        return;
+    }
+
+    sugarData.push(val);
+    updateChart();
+
+    if(val<70){
+        box.className="result low";
+        box.innerHTML="LOW SUGAR - drink juice + rest + see doctor if needed";
+    }
+    else if(val<=140){
+        box.className="result normal";
+        box.innerHTML="NORMAL - keep healthy lifestyle + exercise";
+    }
+    else{
+        box.className="result high";
+        box.innerHTML="HIGH SUGAR - water + avoid sugar + consult doctor if repeated";
+    }
+}
+
+
+/* CHART */
+let chart;
+
+function updateChart(){
+    let ctx=document.getElementById("chart").getContext("2d");
+
+    if(chart) chart.destroy();
+
+    chart=new Chart(ctx,{
+        type:"line",
+        data:{
+            labels:sugarData.map((_,i)=>"Day "+(i+1)),
+            datasets:[{
+                label:"Blood Sugar",
+                data:sugarData,
+                borderColor:"#0f766e",
+                fill:false
+            }]
         }
     });
-
-    // Account data logic
-    if($('#displayEmail').length) {
-        const savedEmail = sessionStorage.getItem("userEmail") || "Guest User";
-        $('#displayEmail').text(savedEmail);
-    }
-});
-
-// Update message function
-function fakeUpdate() {
-    $('#updateMessage').fadeIn().delay(2000).fadeOut();
 }
